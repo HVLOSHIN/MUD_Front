78aa84303e671aad87c45e3f0428e34f7cfb3c5e
HVLOSHIN <halogod9985@naver.com>
2024-11-03 00:46:58 +0900

    feat- stats 링크

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,31 +1,50 @@
 // Header.js
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import './Header.css';
 import { useAuth } from '../../context/AuthContext';
+
 
 const Header = () => {
     const { logout } = useAuth();
     const location = useLocation();
     const whitelistPaths = ['/', '/login', '/signup'];
+    const navigate = useNavigate();
 
     if (whitelistPaths.includes(location.pathname)) {
         return null;
     }
 
+    function goDashboard() {
+        navigate('/dashboard');
+    }
+
+    function goStats() {
+        navigate('/stats');
+    }
+
     return (
         <div className="rpg-header-container">
             <header className="rpg-header">
-                <div className="logo">
-                    <img src="/path/to/logo.png" alt="Logo" />
+                <div className="navbar">
+                    <ul>
+                        <li>
+                            <button onClick={goDashboard}>홈</button>
+                        </li>
+                    </ul>
                 </div>
                 <nav className="navbar">
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Game</a></li>
-                        <li><a href="#">Forum</a></li>
-                        <li><a href="#">Shop</a></li>
-                        <li><a href="#">Contact</a></li>
+                <ul>
+
+                        <li><button onClick={goStats}>상세</button></li>
+                        <li><button onClick={goStats}>훈련</button></li>
+                        <li><button onClick={goStats}>능력</button></li>
+                        <li><button onClick={goStats}>장비</button></li>
+                        <li><button onClick={goStats}>전투</button></li>
+                        <li><button onClick={goStats}>마을</button></li>
+                        <li><button onClick={goStats}>업적</button></li>
+                        <li><button onClick={goStats}>설정</button></li>
+
                     </ul>
                 </nav>
                 <div className="user-info">
